27cd196fc3891ff2aabe85f6f1d57f62d7d40c4a
Zhuo Zhang <imzhuo@foxmail.com>
2022-08-11 10:43:52 +0800

    docs: mention no return value caused behavior

--- overlook.cmake
+++ overlook.cmake
@@ -176,7 +176,8 @@ elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 endif()
 
 # 4. 函数应该有返回值但没有return返回值;或不是所有路径都有返回值
-# 解决bug：lane detect; vpdt for循环无法跳出(android输出trap); -O3时输出内容和其他优化等级不一样（from 三老师）
+# 解决bug：lane detect; vpdt for循环无法跳出(android输出trap); lane calib库读取到随机值导致获取非法格式asvl，开asan则表现为读取NULL指针
+# -O3时输出内容和其他优化等级不一样（from 三老师）
 if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
   overlook_list_append(OVERLOOK_C_FLAGS /we4716 /we4715)
   overlook_list_append(OVERLOOK_CXX_FLAGS /we4716 /we4715)
